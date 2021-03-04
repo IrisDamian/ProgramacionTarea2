4c6d20f6832e6cdb0063fff882d509a75d72d159
IrisDamian <79216183+IrisDamian@users.noreply.github.com>
2021-03-04 11:59:54 -0600

    Examen Parcial1

---
+++ Examen Parcial/Examen Parcial/Adivina tu fecha.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Examen_Parcial
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Adivina_tu_fecha : ContentPage
+    {
+        public Adivina_tu_fecha()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            int result = Convert.ToInt32(resultado.Text);
+            int cifra1, cifra2, cifra3, cifra4;
+            result = result - 365;
+            cifra1 = (result % 10000) / 1000;
+            cifra2 = (result % 10000) % 1000 / 100;
+            cifra3 = ((result % 10000) % 1000) % 100 / 10;
+            cifra4 = (((result % 10000) % 1000) % 100) % 10;
+            string respuesta = ("Muy bien el dia de tu cumpleaños es: " + cifra1 + cifra2 + "y el  mes de tu cumpleaños es:" + cifra3 + cifra4);
+            DisplayAlert("respuesta", respuesta, "Exelente");
+
+        }
+    }
+}
---
+++ Examen Parcial/Examen Parcial/Adivina tu numero.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Examen_Parcial
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Adivina_tu_numero : ContentPage
+    {
+        public Adivina_tu_numero()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            int num = Convert.ToInt32(numero.Text);
+            num = num / 10;
+            num = num - 4;
+            string resul = Convert.ToString(num);
+            DisplayAlert("El numero que imaginaste fue",resul,"Exelente");
+        }
+    }
+}
